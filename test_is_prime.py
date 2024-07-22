d730572f2cab62df41e30dd899b2d814a21c7c1d
ElvisFMM <elvismercado967@gmail.com>
2024-07-22 14:27:14 -0700

    updated is_prime & test

---
+++ test_is_prime.py
@@ -0,0 +1,34 @@
+#file to test function
+
+import unittest
+from is_prime import is_prime
+
+
+class Testing(unittest.TestCase):
+    
+
+    #pass
+    def test_always_passes(self):
+        self.assertTrue(True)
+    #fail
+    def test_always_fails(self):
+        self.assertFalse(False)
+
+    #Non-prime numbers (false)
+    def test_non_prime_numbers(self):
+        self.assertFalse(is_prime(4))
+        self.assertFalse(is_prime(6))
+        self.assertFalse(is_prime(15))
+        self.assertFalse(is_prime(21))
+
+    #Prime numbers (True)
+    def test_prime_numbers(self):
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(3))
+        self.assertTrue(is_prime(5))
+        self.assertTrue(is_prime(7))
+
+
+
+if __name__ == "__main__":
+    unittest.main()
